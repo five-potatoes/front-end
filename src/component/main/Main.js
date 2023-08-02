c1495dfb977d995c085d2c1312f9a8a89fea5412
JuYong <103828756+Noah-JuYong@users.noreply.github.com>
2023-08-03 01:22:19 +0900

    코드 긁어옴. 카메라 작동. 녹화 가능. 보여주기 x. blob객체로 저장되어있음

--- src/component/main/Main.js
+++ src/component/main/Main.js
@@ -1,14 +1,17 @@
 import React from "react";
-import '../styles/Main.css'
+import "../styles/Main.css";
+import Webcam from "../video/Webcam";
 
 const Main = () => {
-    return (
-        <>
-            <img src="logo.png" alt="logo" width={200}/>
-            <div className="test">메인 페이지! 폰트 점검용</div>
-            <div className="default">비교용 기본 폰트</div>
-        </>
-    )
-}
+  return (
+    <>
+      <img src="logo.png" alt="logo" width={200} />
+      <div className="test">메인 페이지! 폰트 점검용</div>
+      <div className="default">비교용 기본 폰트</div>
+
+      <Webcam />
+    </>
+  );
+};
 
 export default Main;
