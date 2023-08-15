b43c05424402f5772c2019c421836deff934062f
JuYong <103828756+Noah-JuYong@users.noreply.github.com>
2023-08-16 01:26:11 +0900

    Update: 버튼 이미지 수정

--- src/component/Home/SignToText.js
+++ src/component/Home/SignToText.js
@@ -34,13 +34,13 @@ const SignToText = () => {
                     </div>
                     <div className={styles.trans}>
                         <textarea className={styles.text} onChange={handleInput} value={text} />
-                        <div>
-                            <button onClick={handleButton} className={styles.btn}>
-                                <img src="img/소리.png"/>
+                        <div className={styles.btn}>
+                            <button onClick={handleButton} >
+                                <img src="img/소리.png" alt="소리" className={ styles.btnImg} />
                             </button>
-                            <CopyToClipboard text={text} className={styles.btn}>
+                            <CopyToClipboard text={text} >
                                 <button className={styles.soundCopy}>
-                                    <img src="img/복사.png"/>
+                                    <img src="img/복사.png" alt="복사" className={ styles.btnImg}/>
                                 </button>
                             </CopyToClipboard>
                         </div>
