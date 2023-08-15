1c66450240688eeb7e2cdb52a46fe46e223ce941
kwon2059 <127478451+kwon2059@users.noreply.github.com>
2023-08-15 13:23:08 +0900

    주용 요구
    ㅈㅇㅇㄱ

--- src/component/Home/Header.js
+++ src/component/Home/Header.js
@@ -26,11 +26,11 @@ const Header = () => {
     );*/
 
 
-
     return (
         <header className={styles.test}>
             <div>
-                <img src="logo.png" alt="logo" className={styles.logo} width={100} />
+                <img src="logo.png" alt="logo" className={styles.logo} width={100}/>
+
                 <button className={styles.headbtn} onClick={goToSign}>수화 번역</button>
                 <button className={styles.headbtn} onClick={goToText}>글자 번역</button>
             </div>  
