3c8ce3968fcf903422057e1282b7227849c17df9
kwon2059 <127478451+kwon2059@users.noreply.github.com>
2023-08-15 11:08:08 +0900

    그냥
    그냥

--- src/App.js
+++ src/App.js
@@ -15,3 +15,6 @@ function App() {
 }
 
 export default App;
+
+//이거 따라서 했는데 browser라우터 같이 못쓴다 오류뜸
+//https://velog.io/@jaeochoiii/React-Router%EC%9D%84-%EC%9D%B4%EC%9A%A9%ED%95%9C-%ED%8E%98%EC%9D%B4%EC%A7%80-%EC%9D%B4%EB%8F%99
